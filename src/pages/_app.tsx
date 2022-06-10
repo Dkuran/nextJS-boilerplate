35ea79400d99ec5a7268b765fd362637eb0a97ac
Dkuran <aduran@devsu.com>
2022-06-09 22:13:57 -0500

    chore: Added redux basic config.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 import '../../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
